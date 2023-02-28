2a7fc2da1f9f7a65ada2f58ed938b57c7a010d69
DESKTOP-SKGP60E\TA <natthaporn@thingsanalytic.com>
2023-02-28 14:04:56 +0700

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import moment from 'moment';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -9,7 +10,7 @@ export class AppComponent {
   public appPages = [
     { title: 'ตำแหน่งจุดตรวจวัด', url: '/location', },//icon: 'map'
     // { title: 'ปริมาณน้ำรายวัน', url: '/level', icon: 'analytics' },
-    { title: 'ปริมาณน้ำรายสัปดาห์', url: '/weekly', }, //icon: 'bar-chart' 
+    // { title: 'ปริมาณน้ำรายสัปดาห์', url: '/weekly', }, //icon: 'bar-chart' 
     // { title: 'ข้อมูลการแจ้งเตือน', url: '/alert', icon: 'warning' },//
   ];
 
@@ -20,9 +21,12 @@ export class AppComponent {
   ];
 
   alerts: any = [];
+  datetime: any = [];
   constructor(private modalCtrl: ModalController) {
     fetch('http://202.129.205.39:3000/api/water/getperson').then(res => res.json()).then(json => {
       this.alerts = json?.person;
+      var dd = moment.locale('th')
+      this.datetime = moment(this.alerts.datetime).add(543, 'year').format('เวลา h:mm:ss  MMMM Do YYYY')
       console.log(this.alerts);
     });
   }
